257f4370ace15d5e964c685d4aa4447798ae7f9e
JoseGalaz12 <jose.galaz@cloud.uautonoma.cl>
2026-06-28 12:54:44 -0400

    Clase nodoLista para lista enlazada

--- src/main/java/com/mycompany/main/Main.java
+++ src/main/java/com/mycompany/main/Main.java
@@ -1,13 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
 
 package com.mycompany.main;
 
-/**
- *
- * @author josem
- */
 public class Main {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/mycompany/main/NodoLista.java
@@ -0,0 +1,13 @@
+
+package com.mycompany.main;
+
+public class NodoLista {
+    Pedido dato;
+    NodoLista siguiente;
+
+    public NodoLista(Pedido dato, NodoLista siguiente) {
+        this.dato = dato;
+        this.siguiente = siguiente;
+    }
+    
+}
